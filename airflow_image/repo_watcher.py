0db678aaaad3f8722620ee9f74d478769a062ade
Thomas La Piana <tlapiana@gitlab.com>
2019-03-04 23:58:35 +0000

    big rewrite to simplify the repo_watcher. now just simply clones once on startup and then pulls every 5 minutes after that.

--- airflow_image/repo_watcher.py
+++ airflow_image/repo_watcher.py
@@ -1,62 +1,37 @@
 #!/usr/local/bin/python
 import logging
 import subprocess
-import time
 from datetime import datetime
+from time import sleep
 
 import feedparser
 
 
 MIN_INTERVAL = 5
-FEED = "https://gitlab.com/gitlab-data/analytics.atom?feed_token=5Zszsskp8pZmr9yy6sGv"
+REPO = "https://gitlab.com/gitlab-data/analytics.git"
 
 
-def watch_repo(interval: int, feed: str) -> None:
+def clone_repo(interval: int, repo: str) -> None:
     """
-    Check an RSS feed for updates.
-    Frequency of checks determined by interval var.
+    Clone a git repo every x minutes.
+
+    interval: determines how often the repo gets cloned
+    repo: https address of the git repo
     """
 
-    # Always clone the repo initially
-    subprocess.run(
-            "git clone https://gitlab.com/gitlab-data/analytics.git analytics",
-            shell=True,
-            check=True,
-    )
+    clone_cmd = f"git clone -b master --single-branch {repo} --depth 1"
+    subprocess.run(clone_cmd.format("clone", repo), shell=True, check=True)
+    print(f"Repo successfully cloned at: {datetime.now()}")
 
-    # Loop and sleep to check for updates
-    last_git_pull = datetime.now()
+    ## TODO: Make this smarter and use merge event web hooks at some point.
     while True:
-        # Find the most recent merge
-        feed = feedparser.parse(feed).entries
-        for item in feed:
-            if "accepted merge request" in  item.title:
-                entry_id = item.id
-                updated_at = item.updated
-                break
-        else:
-            entry_id = feed[0].id
-            updated_at = feed[0].updated
-
-        logging.info("Checking RSS feed...most recent ID is {}".format(entry_id))
-
-        date_format = "%Y-%m-%dT%H:%M:%SZ"
-        last_rss_update = datetime.strptime(updated_at, date_format)
-
-        if last_git_pull < last_rss_update:
-            logging.info("New update found, pulling from master...")
-            subprocess.run(
-                    "git pull",
-                    shell=True,
-                    check=True,
-            )
-            last_git_pull = last_rss_update
-
-        time.sleep(interval * 60)
+        sleep(interval * 60)
+        subprocess.run("cd analytics/ && git pull", shell=True, check=True)
+        print(f"Repo successfully pulled at: {datetime.now()}")
 
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    watch_repo(MIN_INTERVAL, FEED)
+    clone_repo(MIN_INTERVAL, REPO)
