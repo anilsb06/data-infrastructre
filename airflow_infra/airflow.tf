8733c74e0d44ac474f5200b8027654c1d51825c2
Michael Walker <mwalker@gitlab.com>
2021-05-11 15:22:37 -0500

    untested first draft at using terraform for Airflow cluster

---
+++ airflow_infra/airflow.tf
@@ -0,0 +1,101 @@
+provider "google" {
+    project = "gitlab-analysis"
+    region  = "us-west1"
+    zone    = "us-west1-a" 
+}
+
+resource "google_container_cluster" "airflow-cluster" {
+    name     = "${var.environment}-airflow-gke"
+    location = var.region
+    remove_default_node_pool = true
+    initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "highmem-pool" {
+    name        = "${var.environment}-airflow-highmem-pool"
+    cluster     = google_container_cluster.airflow-cluster.name
+    autoscaling {
+        min_node_count = 1
+        max_node_count = 2
+    }
+
+    node_config {
+        machine_type    = "n1-highmem-4"
+        service_account = google_service_account.default.email
+        oauth_scopes    = [
+            "https://www.googleapis.com/auth/cloud-platform"
+        ]
+    }
+}
+
+resource "google_container_node_pool" "production-task-pool" {
+    name    = "${var.environment}-airflow-highmem-pool"
+    cluster = google_container_cluster.airflow_cluster.name
+    autoscaling {
+        min_node_count = 2
+        max_node_count = 5
+    }
+
+    node_config {
+        machine_type    = "n1-highmem-4"
+        service_account = google_service_account.default.email
+        oauth_scopes    = [
+            "https://www.googleapis.com/auth/cloud-platform"
+        ]
+        taint = [
+            {
+                effect = "NO_SCHEDULE",
+                key    = "production",
+                value  = "true"
+            }
+        ]
+    }
+}
+
+resource "google_container_node_pool" "scd" {
+    name    = "${var.environment}-airflow-scd-pool"
+    cluster = google_container_cluster.airflow_cluster.name
+    autoscaling {
+        min_node_count = 1
+        max_node_count = 3
+    }
+
+    node_config {
+        machine_type    = "n1-highmem-4"
+        service_account = google_service_account.default.email
+        oauth_scopes    = [
+            "https://www.googleapis.com/auth/cloud-platform"
+        ]
+        taint = [
+            {
+                effect = "NO_SCHEDULE",
+                key    = "scd",
+                value  = "true"
+            }
+        ]
+    }
+}
+
+resource "google_container_node_pool" "testing-task-pool" {
+    name    = "${var.environment}-airflow-testing-pool"
+    cluster = google_container_cluster.airflow_cluster.name
+    autoscaling {
+        min_node_count = 0
+        max_node_count = 1
+    }
+
+    node_config {
+        machine_type    = "n1-highmem-4"
+        service_account = google_service_account.default.email
+        oauth_scopes    = [
+            "https://www.googleapis.com/auth/cloud-platform"
+        ]
+        taint = [
+            {
+                effect = "NO_SCHEDULE",
+                key    = "test",
+                value  = "true"
+            }
+        ]
+    }
+}
