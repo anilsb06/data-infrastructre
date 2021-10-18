beec13a5faabc090d1997668d68e2258c19bf042
Ved Prakash <vprakash@gitlab.com>
2021-10-18 22:49:33 +0530

    initial commit

--- airflow_infra/GKE/airflow_gcp_gke.tf
+++ airflow_infra/GKE/airflow_gcp_gke.tf
@@ -7,19 +7,53 @@ provider "google" {
 variable "environment" {
     type = string
 }
+variable "network_mode" {
+    type = string
+}
+variable "subnetwork" {
+    type = string 
+}
+variable "min_master_version" {
+  type =  string
+}
 
 resource "google_container_cluster" "airflow_cluster" {
+    provider = google-beta
     name     = var.environment == "production" ? "data-ops" : "data-ops-${var.environment}"
-    network = "gitlab-analysis-vpc"
+    description = var.environment == "production" ? "Production ELT cluster" : "${var.environment} ELT Cluster"
+    network = var.network_mode
     networking_mode = "VPC_NATIVE"
     ip_allocation_policy {
-        cluster_ipv4_cidr_block = "/14"
-        services_ipv4_cidr_block = "/20"
+        cluster_ipv4_cidr_block = "10.180.0.0/14"
+        services_ipv4_cidr_block = "10.182.224.0/20"
     }
     remove_default_node_pool = true
-    subnetwork="gitlab-analysis-subnet"
+    subnetwork=var.subnetwork
     initial_node_count       = 1
-
+    min_master_version = var.min_master_version
+    release_channel {
+      channel="UNSPECIFIED"
+    }
+    maintenance_policy {
+      daily_maintenance_window {
+        start_time="15:00"
+      }
+    }
+    notification_config {
+        pubsub {
+            enabled = false
+        }
+    }
+    vertical_pod_autoscaling {
+      enabled=false
+    }
+    cluster_autoscaling {
+        enabled = true
+      autoscaling_profile = "BALANCED"
+    }
+    private_cluster_config {
+      enable_private_endpoint = false
+    }
 }
 
 resource "google_container_node_pool" "highmem-pool" {
--- airflow_infra/GKE/airflow_gke_staging.tfvars
+++ airflow_infra/GKE/airflow_gke_staging.tfvars
@@ -1,1 +1,4 @@
 environment = "staging-ops"
+network_mode = "default"
+subnetwork= "gke-bizops-runner-subnet-37859e17"
+min_master_version="1.19.13-gke.1200"
